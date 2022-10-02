2a035e6d7c923b4e5bee5c58a50ffc477ec242c4
Gabriel Garcia <garciasholding@gmail.com>
2022-10-02 20:07:09 -0300

    feat: product component

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,11 +1,44 @@
-export function Product({ name, category, price, img }) {
+import { LiCard } from "./style.js";
+
+export function Product({
+    name,
+    category,
+    price,
+    img,
+    handleClick,
+    id,
+    children,
+    removeItemCart,
+    count,
+}) {
     return (
-        <li>
-            <img src={img} alt={name} />
-            <h2>{name}</h2>
-            <p>{category}</p>
-            <p>{price}</p>
-            <button>Adicionar</button>
-        </li>
+        <LiCard>
+            <figure>
+                <img src={img} alt={name} />
+            </figure>
+            <div className="card__container">
+                <div>
+                    <h2>{name}</h2>
+                    <p className="liCard__category">{category}</p>
+                </div>
+                <p className="liCard__price">R${price.toFixed(2)}</p>
+
+                <div className="btn__container">
+                    <button
+                        onClick={(event) => {
+                            if (event.target.innerText === "Adicionar") {
+                                return handleClick(id);
+                            } else {
+                                return removeItemCart(id);
+                            }
+                        }}
+                        className="btn-default"
+                    >
+                        {children}
+                    </button>
+                    <span>{count}</span>
+                </div>
+            </div>
+        </LiCard>
     );
 }
